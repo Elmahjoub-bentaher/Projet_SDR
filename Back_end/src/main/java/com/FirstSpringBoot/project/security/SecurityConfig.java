2bdc33135e80f2f1a7bc13e91397c22009d95fb8
Bouali <youssefbouali206@gmail.com>
2025-06-22 00:00:56 +0100

    Fix CORS

--- Back_end/src/main/java/com/FirstSpringBoot/project/security/SecurityConfig.java
+++ Back_end/src/main/java/com/FirstSpringBoot/project/security/SecurityConfig.java
@@ -15,6 +15,8 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
+import java.util.Arrays;
+import java.util.List;
 import io.github.cdimascio.dotenv.Dotenv;
 
 
