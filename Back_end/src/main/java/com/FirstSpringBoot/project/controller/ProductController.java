e18a283f96bf7b0d56d78c2da1c51992c2fabac0
brahimchakir <ibrahimchakir002@gmail.com>
2025-06-22 13:39:05 +0100

    controller

--- Back_end/src/main/java/com/FirstSpringBoot/project/controller/ProductController.java
+++ Back_end/src/main/java/com/FirstSpringBoot/project/controller/ProductController.java
@@ -13,6 +13,7 @@ import java.util.List;
 @RequestMapping("/api/products")
 //@CrossOrigin(origins = "http://localhost:4200")
 //@CrossOrigin(origins = "http://localhost:3000")
+//@CrossOrigin(origins = "http://localhost:3000")
 public class ProductController {
     private final ProductService productService;
 
