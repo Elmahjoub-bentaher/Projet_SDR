5d094f89bd9c9ff33efcb95e0a55e74cc2b84d13
Bouali <youssefbouali206@gmail.com>
2025-06-22 21:23:27 +0100

    Add commands for livraison

--- front_end/src/pages/Livraisons.tsx
+++ front_end/src/pages/Livraisons.tsx
@@ -1,5 +1,5 @@
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useData } from '@/context/DataContext';
 import { Card } from '@/components/ui/card';
 import { Button } from '@/components/ui/button';
