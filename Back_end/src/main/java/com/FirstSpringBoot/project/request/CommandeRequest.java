deb3b40e8c9b5e6e5f94c00eefbb2fe05bcb3599
Youssef BOUALI <53417796+youssefbouali@users.noreply.github.com>
2025-06-23 10:59:24 +0100

    Update CommandeRequest.java

--- Back_end/src/main/java/com/FirstSpringBoot/project/request/CommandeRequest.java
+++ Back_end/src/main/java/com/FirstSpringBoot/project/request/CommandeRequest.java
@@ -4,14 +4,10 @@ import java.time.LocalDate;
 import java.util.List;
 
 public class CommandeRequest {
-    private LocalDate dateCommande;
     private LocalDate dateLivraisonPrevue;
     private String etat;
     private Long idFournisseur;
     private Long idUtilisateur;
-    
-    private Float montantTotal;
-
     private List<LigneCommandeRequest> lignes;
 
     // Getters and Setters
@@ -54,20 +50,78 @@ public class CommandeRequest {
     public void setLignes(List<LigneCommandeRequest> lignes) {
         this.lignes = lignes;
     }
-
-        public LocalDate getDateCommande() {
-        return dateCommande;
-    }
-
-    public void setDateCommande(LocalDate dateCommande) {
-        this.dateCommande = dateCommande;
-    }
-
-    public Float getMontantTotal() {
-        return montantTotal;
-    }
-
-    public void setMontantTotal(Float montantTotal) {
-        this.montantTotal = montantTotal;
-    }
 }
+
+//package com.FirstSpringBoot.project.request;
+//
+//import java.time.LocalDate;
+//import java.util.List;
+//
+//public class CommandeRequest {
+//    private LocalDate dateCommande;
+//    private LocalDate dateLivraisonPrevue;
+//    private String etat;
+//    private Long idFournisseur;
+//    private Long idUtilisateur;
+//    
+//    private Float montantTotal;
+//
+//    private List<LigneCommandeRequest> lignes;
+//
+//    // Getters and Setters
+//    public LocalDate getDateLivraisonPrevue() {
+//        return dateLivraisonPrevue;
+//    }
+//
+//    public void setDateLivraisonPrevue(LocalDate dateLivraisonPrevue) {
+//        this.dateLivraisonPrevue = dateLivraisonPrevue;
+//    }
+//
+//    public String getEtat() {
+//        return etat;
+//    }
+//
+//    public void setEtat(String etat) {
+//        this.etat = etat;
+//    }
+//
+//    public Long getIdFournisseur() {
+//        return idFournisseur;
+//    }
+//
+//    public void setIdFournisseur(Long idFournisseur) {
+//        this.idFournisseur = idFournisseur;
+//    }
+//
+//    public Long getIdUtilisateur() {
+//        return idUtilisateur;
+//    }
+//
+//    public void setIdUtilisateur(Long idUtilisateur) {
+//        this.idUtilisateur = idUtilisateur;
+//    }
+//
+//    public List<LigneCommandeRequest> getLignes() {
+//        return lignes;
+//    }
+//
+//    public void setLignes(List<LigneCommandeRequest> lignes) {
+//        this.lignes = lignes;
+//    }
+//
+//        public LocalDate getDateCommande() {
+//        return dateCommande;
+//    }
+//
+//    public void setDateCommande(LocalDate dateCommande) {
+//        this.dateCommande = dateCommande;
+//    }
+//
+//    public Float getMontantTotal() {
+//        return montantTotal;
+//    }
+//
+//    public void setMontantTotal(Float montantTotal) {
+//        this.montantTotal = montantTotal;
+//    }
+//}
