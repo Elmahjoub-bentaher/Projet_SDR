864676392d61dd3fa49382f00e59142c9ac0bfe2
Bouali <youssefbouali206@gmail.com>
2025-06-23 10:16:28 +0100

    Fix Commande Create

--- Back_end/src/main/java/com/FirstSpringBoot/project/repository/LigneCommandeRepository.java
+++ Back_end/src/main/java/com/FirstSpringBoot/project/repository/LigneCommandeRepository.java
@@ -4,6 +4,8 @@ import com.FirstSpringBoot.project.model.LigneCommande;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface LigneCommandeRepository extends JpaRepository<LigneCommande, Long> {
     // Par exemple :
